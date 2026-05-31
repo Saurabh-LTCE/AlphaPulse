45b3b04021f6568e6d160dd0638734c0dfc220ba
Saurabh-LTCE <saurabhsharma13052004@gmail.com>
2026-05-31 17:34:51 +0530

    Create missig files&folder and implement React router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import HomePage from './landing_page/Home/HomePage';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
 
+import HomePage from "./landing_page/Home/HomePage";
+import Signup from "./landing_page/Signup/Signup";
+import AboutPage from "./landing_page/About/AboutPage";
+import ProductPage from "./landing_page/Products/ProductsPage";
+import PricingPage from "./landing_page/Pricing/PricingPage";
+import SupportPage from "./landing_page/Support/SupportPage";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import NotFound from "./landing_page/NotFound";
+import Navbar from "./landing_page/Navbar";
+import Footer from "./landing_page/Footer";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <HomePage />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/Signup" element={<Signup />} />
+      <Route path="/About" element={<AboutPage />} />
+      <Route path="/Product" element={<ProductPage />} />
+      <Route path="/Pricing" element={<PricingPage />} />
+      <Route path="/Support" element={<SupportPage />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+    <Footer />
+  </BrowserRouter>
 );
---
+++ frontend/src/landing_page/Pricing/PricingPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "./Hero";
+import OpenAccount from "../OpenAccount";
+
+function PricingPage() {
+  return (
+    <>
+      <Hero />
+      <CreateTicket />
+    </>
+  );
+}
+
+export default PricingPage;
